7f963ccb498eeff1deb448d1f48d50fcc7877a1f
小野湧太郎 <ono.yutaro.tec0623@gmail.com>
2019-10-12 19:52:11 +0900

    エネミーの追尾移動を実装

--- MineExplorer/Assets/Script/Enemy/EnemyMovement.cs
+++ MineExplorer/Assets/Script/Enemy/EnemyMovement.cs
@@ -1,18 +1,36 @@
-﻿using System.Collections;
+﻿//-------------------------------------------------------//
+// エネミーの移動処理 プレイヤーの追跡(NavMesh使用)
+//                                    2019 Yutaro Ono.
+//-----------------------------------------------------//
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
+    // NavMeshAgentの取得
+    private NavMeshAgent myNav;
+
+    // ターゲティングするプレイヤーオブジェクト格納用
+    private GameObject tagPlayer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // 自身のNavMeshAgentの取得
+        myNav = gameObject.GetComponent<NavMeshAgent>();
+
+        // プレイヤーオブジェクトを取得
+        tagPlayer = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // ターゲット(プレイヤー位置)に自動移動する
+        myNav.destination = tagPlayer.transform.position;
     }
+
+    
 }
