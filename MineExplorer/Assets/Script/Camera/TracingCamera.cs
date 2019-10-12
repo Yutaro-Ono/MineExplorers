640633d61a3e4629398f71fd47c4e802be731767
花田　空 <ds215323@gmail.com>
2019-10-12 17:39:49 +0900

    Cameraのプレハブの作成、追従するCameraのスクリプトの作成（角度は変わらない）、PlayerのプレハブからCameraの削除

---
+++ MineExplorer/Assets/Script/Camera/TracingCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TracingCamera : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //y10,z-10
+        transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y + 10, Player.transform.position.z - 10);
+    }
+}
