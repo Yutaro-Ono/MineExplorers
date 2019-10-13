f21c26859a963a5f3dce2739877bf796984bd157
urotayu <ono.yutaro.tec0623@gmail.com>
2019-10-13 15:56:13 +0900

    エネミーのプレイヤーに対する攻撃判定を追加。

--- MineExplorer/Assets/Script/Enemy/EnemyAttack.cs
+++ MineExplorer/Assets/Script/Enemy/EnemyAttack.cs
@@ -12,14 +12,30 @@ public class EnemyAttack : MonoBehaviour
     public bool isAttack;
 
     // 攻撃判定用のサーチエリア
-    // private SphereCollider m_attackArea;
+    [SerializeField] GameObject m_attackPoint;
 
     // Start is called before the first frame update
     void Start()
     {
         isAttack = false;
 
-        // m_attackArea = GetComponent<SphereCollider>();
+        // 攻撃当たり判定についてはオフにしておく
+        m_attackPoint.SetActive(false);
+    }
+
+    void Update()
+    {
+
+        // 攻撃フラグがたったら当たり判定をオン
+        if(isAttack == true)
+        {
+            m_attackPoint.SetActive(true);
+        }
+        else
+        {
+            m_attackPoint.SetActive(false);
+        }
+
     }
 
     // プレイヤーとの衝突判定
