302fc4bb41e57ee6d7deaceefe998c8f72fe6af5
yutaro <ono.yutaro.tec0623@gmail.com>
2019-10-26 16:27:11 +0900

    デバッグ用フリーカメラ追加。プレイヤーがひたすらしゃがむバグが発生したので、Controllerをいじったが効果なし

---
+++ MineExplorer/Assets/Script/Enemy/EnemySpawnSystem.cs
@@ -0,0 +1,31 @@
+﻿// エネミーをゲーム中、動的に生成する
+//            2019 Yutaro Ono.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnSystem : MonoBehaviour
+{
+    // スポーンさせるエネミー格納
+    [SerializeField] GameObject[] m_enemyObj;
+
+    // 現在フィールドにいるエネミーの総数
+    public int nowEnemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnEnemy(int in_enemyType, int in_spawnPoint)
+    {
+
+    }
+}
